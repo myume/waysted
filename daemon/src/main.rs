98274052508c3938e4e1843ee3e366d1518b3e26
myu <contact@memyu.com>
2025-11-11 11:38:44 -0500

    refactor: top level error handling to log errors

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,17 +1,30 @@
-use log::info;
+use std::process::ExitCode;
+
+use log::{error, info};
 
 use crate::daemon::Daemon;
 
 mod daemon;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> ExitCode {
     env_logger::init();
 
     info!("Starting waysted daemon...");
 
-    let mut daemon = Daemon::new()?;
-    daemon.start()?;
+    match Daemon::new() {
+        Ok(mut daemon) => {
+            if let Err(err) = daemon.start() {
+                error!("{err}");
+                return ExitCode::FAILURE;
+            }
+        }
+        Err(err) => {
+            error!("Failed to initialize daemon: {err}");
+            return ExitCode::FAILURE;
+        }
+    }
 
     info!("Stopping waysted daemon.");
-    Ok(())
+
+    ExitCode::SUCCESS
 }
