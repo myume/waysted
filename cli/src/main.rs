2ce79b3ddf5726d5ce4cd58e8303602383578df8
myu <contact@memyu.com>
2025-11-11 22:14:40 -0500

    refactor: date parsing to reduce duplicate code + add docs

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -11,64 +11,69 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
+    /// Query Screentime data
     Screentime {
-        #[arg(value_parser = DateQuery::parse_date_query)]
-        date_query: DateQuery,
+        #[arg(value_parser = DateRange::parse_date_query)]
+        /// The range of dates to retrieve screentime from: one of `today`, `yesterday`, `YYYY-MM-DD` or `YYYY-MM-DD to YYYY-MM-DD`
+        date_range: DateRange,
     },
+
+    /// Clear collected screentime from database
     Clear,
 }
 
 #[derive(Debug, Clone)]
-enum DateQuery {
-    Range {
-        start: DateTime<Utc>,
-        end: DateTime<Utc>,
-    },
+struct DateRange {
+    start: DateTime<Utc>,
+    end: DateTime<Utc>,
 }
 
-impl DateQuery {
-    fn parse_date_query(s: &str) -> Result<DateQuery, String> {
+impl DateRange {
+    fn parse_date_query(s: &str) -> Result<DateRange, String> {
+        let range = match s.to_lowercase().as_str() {
+            "today" => {
+                let date = Utc::now();
+                Ok(DateRange {
+                    start: date,
+                    end: date,
+                })
+            }
+            "yesterday" => {
+                let yesterday = Utc::now() - Days::new(1);
+                Ok(DateRange {
+                    start: yesterday,
+                    end: yesterday,
+                })
+            }
+            s => {
+                let date_re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+                let date_range_re =
+                    Regex::new(r"^(?<from>\d{4}-\d{2}-\d{2}) to (?<to>\d{4}-\d{2}-\d{2})$")
+                        .unwrap();
+
+                if date_re.is_match(s) {
+                    let date = DateRange::parse_ymd_to_datetime(s);
+                    Ok(DateRange {
+                        start: date,
+                        end: date,
+                    })
+                } else if date_range_re.is_match(s) {
+                    let dates = date_range_re.captures(s).unwrap();
+                    let start = DateRange::parse_ymd_to_datetime(&dates["from"]);
+                    let end = DateRange::parse_ymd_to_datetime(&dates["to"]);
+                    Ok(DateRange { start, end })
+                } else {
+                    Err("\ndate_range must be in the form of `today`, `yesterday`, `YYYY-MM-DD` or `YYYY-MM-DD to YYYY-MM-DD`".to_string())
+                }
+            }
+        };
+
         let day_start = NaiveTime::MIN;
         let day_end = NaiveTime::from_hms_opt(23, 59, 59).unwrap();
-        if s.to_lowercase() == "today" {
-            let date = Utc::now();
-            return Ok(DateQuery::Range {
-                start: date.with_time(day_start).unwrap(),
-                end: date.with_time(day_end).unwrap(),
-            });
-        }
-
-        if s.to_lowercase() == "yesterday" {
-            let yesterday = Utc::now() - Days::new(1);
-            return Ok(DateQuery::Range {
-                start: yesterday.with_time(day_start).unwrap(),
-                end: yesterday.with_time(day_end).unwrap(),
-            });
-        }
-
-        let date_re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
-        if date_re.is_match(s) {
-            let date = DateQuery::parse_ymd_to_datetime(s);
-            return Ok(DateQuery::Range {
-                start: date.with_time(day_start).unwrap(),
-                end: date.with_time(day_end).unwrap(),
-            });
-        }
-
-        let date_range_re =
-            Regex::new(r"^(?<from>\d{4}-\d{2}-\d{2}) to (?<to>\d{4}-\d{2}-\d{2})$").unwrap();
-        if date_range_re.is_match(s) {
-            let dates = date_range_re.captures(s).unwrap();
-            let start = DateQuery::parse_ymd_to_datetime(&dates["from"])
-                .with_time(day_start)
-                .unwrap();
-            let end = DateQuery::parse_ymd_to_datetime(&dates["to"])
-                .with_time(day_end)
-                .unwrap();
-            return Ok(DateQuery::Range { start, end });
-        }
-
-        Err("\ndate_query must be in the form of `today`, `yesterday`, `YYYY-MM-DD` or `YYYY-MM-DD to YYYY-MM-DD`".to_string())
+        range.map(|range| DateRange {
+            start: range.start.with_time(day_start).unwrap(),
+            end: range.end.with_time(day_end).unwrap(),
+        })
     }
 
     fn parse_ymd_to_datetime(ymd: &str) -> DateTime<Utc> {
@@ -84,7 +89,9 @@ fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Screentime { date_query } => {
+        Commands::Screentime {
+            date_range: date_query,
+        } => {
             println!("{:?}", date_query);
         }
         Commands::Clear => todo!(),
