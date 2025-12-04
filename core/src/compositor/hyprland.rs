73e3e1711b4e0c10b985063e580aed05f8d6834a
Julien Bertazzo Lambert <jbertazzolambert@gmail.com>
2025-12-03 22:56:43 -0500

    feat: add hyprland support

--- core/src/compositor/hyprland.rs
+++ core/src/compositor/hyprland.rs
@@ -1,6 +1,10 @@
 use std::io;
 
 use crate::compositor::Compositor;
+use hyprland::data::Client;
+use hyprland::event_listener::EventListener;
+use hyprland::shared::HyprDataActiveOptional;
+use log::error;
 
 pub struct Hyprland {}
 
@@ -12,13 +16,36 @@ impl Hyprland {
 
 impl Compositor for Hyprland {
     fn get_focused_window(&mut self) -> Result<super::WindowInfo, String> {
-        todo!()
+        match Client::get_active() {
+            Ok(Some(client)) => Ok(super::WindowInfo {
+                title: client.title,
+                app_name: client.class.to_string(),
+            }),
+            Ok(None) => Err(format!("No window has focus in Hyprland")),
+            Err(err) => Err(format!("Failed to get focused window from Hyprland: {err}")),
+        }
     }
 
     fn watch_focused_window(
         &mut self,
         sender: std::sync::mpsc::Sender<super::WindowInfo>,
     ) -> std::io::Result<()> {
-        todo!()
+        let mut event_listener = EventListener::new();
+        event_listener.add_active_window_changed_handler(move |data| match data {
+            Some(window_event) => {
+                if let Err(err) = sender.send(super::WindowInfo {
+                    title: window_event.title,
+                    app_name: window_event.class.to_string(),
+                }) {
+                    error!("Failed to send window info: {err}");
+                };
+            }
+            None => {
+                error!("New focused window is missing.");
+            }
+        });
+
+        event_listener.start_listener().unwrap();
+        Ok(())
     }
 }
